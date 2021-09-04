3d2b4db487cd67ecda68f534de1e242728ce1fb1
Ken Jiang <jiangk@bxscience.edu>
2021-09-04 17:15:25 -0400

    Add Django Rest Framework

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth.models import User
+from rest_framework import routers
+from profiles.viewsets import ProfileViewSet,LanguageViewSet
+
+router = routers.DefaultRouter()
+router.register(r'profiles', ProfileViewSet)
+router.register(r'languages', LanguageViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
