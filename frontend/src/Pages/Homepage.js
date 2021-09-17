5c6e72708c46690f50a0233d81f1c0fa468a2eba
huangbaron2 <44422867+huangbaron2@users.noreply.github.com>
2021-09-17 14:08:03 -0400

    Add dropdown menu of multiple locations to the homepage #19
    * Switched from next.js to react.js for frontend, added minimum functions homepage, browse locations page, and specific location page
    
    * Added viewset for User model
    
    * installed react-bootstrap and changed clickable locations to dropdown

---
+++ frontend/src/Pages/Homepage.js
@@ -0,0 +1,73 @@
+import { Component } from "react"
+import './Homepage.css'
+import { Link , BrowserRouter as Router } from 'react-router-dom';
+import { DropdownButton, Dropdown, Button } from 'react-bootstrap'
+
+class Homepage extends Component {
+
+  constructor(props) {
+
+    /*The locations state is currently containing placeholders until database is set up*/
+    super(props);
+    this.state = {
+      locations: ['location_1', 'location_2'],
+      users: null
+    }; 
+  }
+  
+  componentDidMount() {
+    /*
+    fetch("http://localhost:8000/users/", 
+    {
+      method: "GET",
+      mode: 'cors',
+      headers: {
+        'Access-Control-Allow-Origin':'http://localhost:8000'
+      }
+    })
+    .then(
+      (result) => {
+        alert("Fetching Successful")
+        this.setState({
+          users: result
+        });
+      },
+      (error) => {
+        alert(error)
+      }
+    )
+    */
+  } 
+  
+  render(){
+    return (
+      <div>
+          <h1>Welcome to the Traveler Homepage</h1>
+          <div>
+            <h1>Locations</h1>
+            {/* 
+              <div id = "For_All_Location">
+                {this.state.locations.map((locations, index) => (
+                    <div className = "Location_Boxes">
+                      <Link className = "Location_Click" to = {'/locations/' + locations}>{locations}</Link>
+                    </div>
+                ))}
+              </div>
+            */}
+              <div>
+              <DropdownButton id="dropdown-basic-button" title="Choose your location">
+                {this.state.locations.map((locations, index) => (
+                    <div className = "Location_Boxes">
+                      <Dropdown.Item href = {'/locations/' + locations}>{locations}</Dropdown.Item>
+                    </div>
+                ))}
+              </DropdownButton>
+              </div>
+          </div>
+          <h1>users from fetch: {this.state.users}</h1>
+      </div>
+    );
+  }
+}
+
+export default Homepage;
---
+++ frontend/src/Routes.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Homepage from './Pages/Homepage'
+import Browse_Locations from './Pages/Browse_Locations'
+import Specific_Location from './Pages/Specific_Location'
+
+
+const Routes = () => {
+    return (
+      <Switch> {/* The Switch decides which component to show based on the current URL.*/}
+        <Route exact path='/' component={Homepage}></Route>
+        <Route exact path='/locations' component={Browse_Locations}></Route>
+        <Route path='/locations/:id' component={Specific_Location}></Route>
+      </Switch>
+    );
+  }
+
+export default Routes
