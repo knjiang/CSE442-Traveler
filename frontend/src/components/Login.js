27d3cc00faa36d600f2f73f9d4dc03fa44399eb7
Ken Jiang <jiangk@bxscience.edu>
2021-09-26 18:45:33 -0400

    resolve merge conflicts

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,32 +1,13 @@
 import React from 'react';
 import GoogleLogin from 'react-google-login';
 import { useCookies } from 'react-cookie';
-<<<<<<< HEAD
 import { loginBackend} from '../apis/auth';
-=======
->>>>>>> 3e1f2d283607283cee1a972783b8d0f9bc999b5a
 
 function Login(){
     const [cookies,setCookie] = useCookies(['token']);
 
     const onSuccess = (res) => {
-<<<<<<< HEAD
         loginBackend(res.accessToken)
-=======
-        fetch("http://localhost:8000/dj-rest-auth/google/",
-        {
-            headers: {
-              'Accept': 'application/json',
-              'Content-Type': 'application/json'
-            },
-            method: "POST",
-            body: JSON.stringify({
-              "access_token": res.accessToken,
-              "code": "",
-              "id_token": "",
-          })
-        })
->>>>>>> 3e1f2d283607283cee1a972783b8d0f9bc999b5a
         .then(response => response.json())
         .then(data => {
             setCookie('token', data.key, { path: '/' });
