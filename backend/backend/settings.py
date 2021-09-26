9f61ff4d99625bc8362c8a24590828a837b3c07e
Anthony Hom <anthonyhom4@gmail.com>
2021-09-26 19:56:31 -0400

    Removed cors headers and whitelist to get updated code from development branch

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'corsheaders',
     'profiles.apps.ProfilesConfig',
 ]
 
@@ -126,7 +125,3 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-CORS_ORIGIN_WHITELIST = [
-     'http://localhost:3000'
-]
