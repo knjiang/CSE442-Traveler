a9ebd9ce7b22d5cfe43d76083b9a2b6ec95bd511
kingfrank123 <47665274+kingfrank123@users.noreply.github.com>
2021-09-27 01:36:11 -0400

    Add frontend functionality to the forum button (#29)
    * add backend models to Forums
    
    * add frontend to Forums
    
    * remove .pyc files
    
    * package change lock version from 2 to 1

---
+++ frontend/src/Pages/Forum.js
@@ -0,0 +1,12 @@
+
+
+function Forum(){
+    return(<div>
+        <h1>Welcome to Forum Page</h1>
+            <button id="home" ><a href = "/"> Return to HomePage </a></button>
+    </div>
+
+    )
+}
+
+export default Forum;
