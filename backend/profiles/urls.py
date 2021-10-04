f5a2337574790316cf4bf47d91af181578891702
anthonyhom2 <47641286+anthonyhom2@users.noreply.github.com>
2021-10-04 01:01:45 -0400

    Add an immediate search and filter to clearly find a specific user #47
    Co-authored-by: Anthony Hom <anthonyhom4@gmail.com>

--- backend/profiles/urls.py
+++ backend/profiles/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import GetProfileView, ChangeLocationView, ChangeListView, SearchUserView
+from .views import GetProfileView, ChangeLocationView, ChangeListView, SearchUserView, GetAllProfilesView
 
 app_name = "profiles"
 
@@ -8,5 +8,6 @@ urlpatterns = [
     path('get_profile/', GetProfileView.as_view()),
     path('change_location/', ChangeLocationView.as_view()),
     path('change_list', ChangeListView.as_view()),
-    path('search_user/', SearchUserView.as_view())
+    path('search_user/', SearchUserView.as_view()),
+    path('search_filter/', GetAllProfilesView.as_view())
 ]
