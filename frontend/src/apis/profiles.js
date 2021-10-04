91d93caa8d9475db8354bcdeae668404250dae81
Ken Jiang <39507362+knjiang@users.noreply.github.com>
2021-10-04 01:44:06 -0400

    fix frontend compilation error #47 (#53)

--- frontend/src/apis/profiles.js
+++ frontend/src/apis/profiles.js
@@ -54,11 +54,16 @@ const changeList = async(token,name,list) => {
         }),
     })
 }
+const getQuery = (e,email) => {
+    e.preventDefault()
+    const encoded_input = encodeURIComponent(email)
+    return fetch(`${BASE_URL}/api/profiles/search_user/?user_email=${encoded_input}`, {
+    method: "GET",
+    }) 
+}
 
 const getUserList = () => {
-    return fetch(`${BASE_URL}/api/profiles/search_filter`, {
-        method: "GET",
-        }) 
+    return fetch(`${BASE_URL}/api/profiles/search_filter/`) 
 }
 
 export {getProfile, changeLocation, getQuery, changeList, getList, getUserList}
