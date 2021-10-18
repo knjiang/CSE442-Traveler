267f5a54c410ca006c989e9f23fec84ac0165a24
Brian C <brianc@Brians-MacBook-Pro.local>
2021-10-17 21:42:46 -0400

    Finished creating user profile page and made page react with a user's information #10 #59

--- frontend/src/Pages/UserProfile.js
+++ frontend/src/Pages/UserProfile.js
@@ -3,8 +3,12 @@ import { changeBackground, getProfile } from '../apis/profiles';
 import { useState, useEffect } from "react"
 import NotLoggedIn from '../components/NotLoggedIn';
 import { useTextInput } from '../hooks/text-input';
+import { StyleHTMLAttributes } from 'react';
 
 function UserProfile() {
+
+
+
   const [cookies, setCookie] = useCookies(['token']);
 
   const [dataList, setList] = useState({
@@ -51,25 +55,28 @@ function UserProfile() {
   if (existsCookie) {
     return (
       <div>
-        <h1>Welcome {user.name} </h1>   <br />
+        <h1 style={{textAlign:'center', color:(214, 122, 127)}}>Welcome {user.name} </h1>
 
-        <h2> About Me</h2>
-        Name: {user.name} <br />
-        Email: {user.email} <br />
-        Location: {user.from_location} <br />
-        Background: {user.background}
+        <h2 style={{textAlign:'center'}}> About Me</h2>
+        <ul style={{textAlign:'center',listStyle: 'none'}}>
+        <li>Name: {user.name}</li>
+        <li>Email: {user.email} </li>
+        <li>Location: {user.from_location}</li>
+        </ul>
+
+        <h2 style={{textAlign:'center'}}>Background and Interests</h2>
+        <p style={{textAlign:'center'}}>{user.background}</p>
         <br /> <br />
-
-        <form onSubmit={(e) => handleSubmit(e)}>
+        <form onSubmit={(e) => handleSubmit(e)} style={{textAlign:'center'}}>
           <label>
-            Enter your background Info
+            <h5 style={{textAlign:'center'}}>Change/Add your background and interests in the text box below</h5>
+            <h5 style={{textAlign:'center'}}>Submit and refresh browser to see applied changes</h5>
             <br />
             <p>Background/Interests: <input type="text" {...backgroundInfoBind} /></p>
-            <br />
           </label>
           <input type="submit" value="Submit" />
         </form>
-        Background: {backgroundInfo}
+        <p style={{textAlign:'center'}}>{backgroundInfo}</p>
       </div>
     )
   }
