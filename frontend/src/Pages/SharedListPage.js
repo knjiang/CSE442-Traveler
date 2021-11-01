8a184616b7f678bcbe0d64d75c33256f0b491416
Frank <frankyan@buffalo.edu>
2021-11-01 18:22:25 -0400

    #107 added descriptions to be displayed on SharedListPage.js

--- frontend/src/Pages/SharedListPage.js
+++ frontend/src/Pages/SharedListPage.js
@@ -1,10 +1,15 @@
 import {useState,useEffect} from "react"
 import {getShareableContents} from "../apis/locations"
+import {getDescription} from "../apis/profiles"
+import { useCookies } from "react-cookie"
 
 function SharedListPage(){
-    const [title, setTitle] = useState("")
+    const [title, setTitle] = useState(false)
     const [author, setAuthor] = useState("")
     const [dataList,setDataList] = useState([])
+    const [descriptions,setDescriptions] = useState(false)
+    const [cookies,setCookie] = useCookies(['token']);
+
 
     useEffect(() => {
         const pathname = window.location.pathname.substr(7)
@@ -16,7 +21,28 @@ function SharedListPage(){
             setAuthor(data.created_by)
             setDataList(data.locations)
         })
+
       }, [])
+
+      useEffect(() => {
+        if(title){
+            console.log(title)
+            getDescription(cookies.token,title)
+        
+        .then(res => res.json())
+        .then(data => {
+            if (data["listDescriptions"].length < 1){
+                setDescriptions("There is no avaliable description")
+            }
+            else {
+
+                setDescriptions(data["listDescriptions"])
+            }
+        })
+        }
+      })
+
+    
 
     const returnLocations = () => {
         let res = [<div style = {{"borderBottom": "2px solid gray", "display":"flex"}}><h2 style = {{"fontSize": "4vh", "paddingBottom": "1vh", "paddingTop": "1vh", "marginLeft": "auto", "marginRight": "auto"}}>Your Locations </h2></div>]
@@ -28,7 +54,9 @@ function SharedListPage(){
     return(
         <div>
         <h1> {title} - by {author} </h1>
+        <div style={{"fontWeight":"800", "fontSize":"5.5vh"}}>{descriptions} </div>
         {returnLocations()}
+        
         </div>
     )
 }
