4d167d7d561831f75125d9dd173c9cb861f46f85
Ken Jiang <jiangk@bxscience.edu>
2021-11-05 00:49:19 -0400

    Fix daphne server deployment #105

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -8,16 +8,17 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.dev')
+django_asgi_app = get_asgi_application()
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
 from chat.routing import urlpatterns
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
-
 application = ProtocolTypeRouter({
-  "http": get_asgi_application(),
+  "http": django_asgi_app,
   "websocket": AuthMiddlewareStack(
         URLRouter(
             urlpatterns
