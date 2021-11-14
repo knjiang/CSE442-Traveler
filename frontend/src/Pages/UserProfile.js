62b8044b1930c9afba5b78ef5db4cc971368fe88
Brian <briancis@buffalo.edu>
2021-11-14 14:44:10 -0500

    Created 2 separate renders for profile #126

--- frontend/src/Pages/UserProfile.js
+++ frontend/src/Pages/UserProfile.js
@@ -1,6 +1,6 @@
 import { useCookies } from 'react-cookie';
 import { changeBackground, changeVisited } from '../apis/profiles';
-import { getProfile, getListData , addLocationList, addList, deleteList, deleteLocationList, getSetShareableLink } from '../apis/profiles';
+import { getProfile, getListData, addLocationList, addList, deleteList, deleteLocationList, getSetShareableLink } from '../apis/profiles';
 import { useState, useEffect } from "react"
 import NotLoggedIn from '../components/NotLoggedIn';
 import { useTextInput } from '../hooks/text-input';
@@ -13,37 +13,39 @@ import BottomVisited from '../components/BottomVisited';
 function UserProfile(props) {
 
   const user = props.parentUser
-  const setUser = props.parentSetUser 
-  const [cookies,setCookie] = useCookies(['token']);
-  const [dataList,setList] = useState()
+  const setUser = props.parentSetUser
+  const [cookies, setCookie] = useCookies(['token']);
+  const [dataList, setList] = useState()
   const existsCookie = typeof cookies.token != "undefined"
   const parentData = useLocation()
   const [selectedList, selectList] = useState()
   const [allLocation, setAllLocation] = useState()
   const [showShareList, setShareListModal] = useState(false)
-  const [shareLink, setShareLink] = useState("")
-  
+  const [shareLink, setShareLink] = useState("") 
+
+  const [isInEditMode, setIsInEditMode] = useState(false)
+
   useEffect(() => {
-    if (existsCookie){
-        getListData(cookies.token)
+    if (existsCookie) {
+      getListData(cookies.token)
         .then(response => response.json())
         .then(data => {
-            setList(data["lists"])
+          setList(data["lists"])
         });
-        getLocation()
+      getLocation()
         .then(response => response.json())
         .then(data => {
-          if (data){
-            setAllLocation(data.map(({id, name}) => name))
+          if (data) {
+            setAllLocation(data.map(({ id, name }) => name))
           }
         })
-      }
+    }
   }, [])
 
   useEffect(() => {
-    if (parentData.state && dataList){
-        selectList(parentData.state)
-        parentData.state = false
+    if (parentData.state && dataList) {
+      selectList(parentData.state)
+      parentData.state = false
     }
   }, [dataList])
 
@@ -52,32 +54,32 @@ function UserProfile(props) {
   }
 
   const shareList = (name) => {
-    getSetShareableLink(cookies.token,name)
-    .then(response => response.json())
-    .then(data => {
+    getSetShareableLink(cookies.token, name)
+      .then(response => response.json())
+      .then(data => {
         setShareLink(data.url)
         setShareListModal(true)
-    });
+      });
   }
 
 
   const returnListName = () => {
-      if (typeof dataList != 'undefined'){
-          let res = [<div style = {{"borderBottom": "2px solid gray", "display":"flex"}}><h1 style = {{"fontSize": "4vh", "paddingBottom": "1vh", "paddingTop": "1vh", "marginLeft": "auto", "marginRight": "auto"}}>Your Lists </h1></div>]
-          for (let name of Object.keys(dataList)){
-              if (name == selectedList){
-                  res.push(<h1 id = "nameTextSelected" onClick = {() => (selectList(name))}>{name} <Button variant="secondary" id = "shareBTN" onClick = {(evt) => (evt.stopPropagation(),shareList(name))}> Share </Button> </h1>)
-              }
-              else {
-                  res.push(<h1 id = "nameText" onClick = {() => (selectList(name))}>{name} <Button variant="secondary" id = "shareBTN" onClick = {(evt) => (evt.stopPropagation(),shareList(name))}> Share </Button></h1>)
-              }
+    if (typeof dataList != 'undefined') {
+      let res = [<div style={{ "borderBottom": "2px solid gray", "display": "flex" }}><h1 style={{ "fontSize": "4vh", "paddingBottom": "1vh", "paddingTop": "1vh", "marginLeft": "auto", "marginRight": "auto" }}>Your Lists </h1></div>]
+      for (let name of Object.keys(dataList)) {
+        if (name == selectedList) {
+          res.push(<h1 id="nameTextSelected" onClick={() => (selectList(name))}>{name} <Button variant="secondary" id="shareBTN" onClick={(evt) => (evt.stopPropagation(), shareList(name))}> Share </Button> </h1>)
+        }
+        else {
+          res.push(<h1 id="nameText" onClick={() => (selectList(name))}>{name} <Button variant="secondary" id="shareBTN" onClick={(evt) => (evt.stopPropagation(), shareList(name))}> Share </Button></h1>)
+        }
 
-          }
-          return(res)
       }
+      return (res)
+    }
   }
-  
-  const [visitedCounttries, setVisitedCountries] = useState("")
+
+  // const [visitedCounttries, setVisitedCountries] = useState("")
 
   const { value: backgroundInfo, bind: backgroundInfoBind, reset: resetBackgroundInfo } = useTextInput('')
   const { value: visitedInfo, bind: visitedInfoBind, reset: resetVisitedInfo } = useTextInput('')
@@ -85,41 +87,52 @@ function UserProfile(props) {
   const handleSubmit = (e) => {
     e.preventDefault()
     changeBackground(cookies.token, backgroundInfo)
-    setUser(prevState => ({...prevState, background : backgroundInfo}))
+    setUser(prevState => ({ ...prevState, background: backgroundInfo }))
   }
 
   const handleSubmit2 = (b) => {
     b.preventDefault()
     changeVisited(cookies.token, visitedInfo)
-    setUser(prevState => ({...prevState, visited : visitedInfo}))
+    setUser(prevState => ({ ...prevState, visited: visitedInfo }))
   }
 
+  const handleEdit = () => {
+    setIsInEditMode(true)
+  }
 
-  if (existsCookie) {
+  const handleDone = () => {
+    setIsInEditMode(false)
+  }
+
+  const returnEditMode = () => {
     return (
       <div>
         <h1 style={{ textAlign: 'center', color: (214, 122, 127) }}>Welcome {user.name} </h1>
         <br />
 
-        <h5 style={{ textAlign: 'center', textDecoration: 'underline'}}> About Me</h5>
-        <div style = {{ textAlign: 'center'}}>
-        <ul style = {{ textAlign: 'left', display: 'inline-block'}}>
-          <li>Name: {user.name}</li>
-          <li>Email: {user.email} </li>
-          <li>Location: {user.from_location}</li>
-        </ul>
+
+        <form onSubmit={() => handleDone()} style={{ textAlign: 'center' }}>
+          <input type="submit" value="Done" />
+        </form>
+
+        <h5 style={{ textAlign: 'center', textDecoration: 'underline' }}> About Me</h5>
+        <div style={{ textAlign: 'center' }}>
+          <ul style={{ textAlign: 'left', display: 'inline-block' }}>
+            <li>Name: {user.name}</li>
+            <li>Email: {user.email} </li>
+            <li>Location: {user.from_location}</li>
+          </ul>
         </div>
 
-        <p style={{textAlign: 'center' , fontStyle: 'italic'}}>For the following text fields, please enter the information you want to display and press Submit <br />
-        "Refresh Browser to see changes applied"</p>
-      
-        {/* This is for background */}
+        <p style={{ textAlign: 'center', fontStyle: 'italic' }}>For the following text fields, please enter the information you want to display and press Submit <br />
+          "Refresh Browser to see changes applied"</p>
+
         <form onSubmit={(e) => handleSubmit(e)} style={{ textAlign: 'center' }}>
 
-        <h2 style={{textAlign:'center'}}>Background and Interests</h2>
-        <p style={{textAlign:'center'}}>{user.background}</p>
+          <h5 style={{ textAlign: 'center', textDecoration: 'underline' }}>Background and Interests</h5>
+          <p style={{ textAlign: 'center' }}>{user.background}</p>
         </form>
-        <form onSubmit={(e) => handleSubmit(e)} style={{textAlign:'center'}}>
+        <form onSubmit={(e) => handleSubmit(e)} style={{ textAlign: 'center' }}>
           <label>
             <br />
             <p>Change Background: <input type="text" {...backgroundInfoBind} /></p>
@@ -128,7 +141,6 @@ function UserProfile(props) {
         </form>
         <p style={{ textAlign: 'center' }}>{backgroundInfo}</p>
 
-        {/* This is for Recommendations for favorite countries */}
         <h5 style={{ textAlign: 'center', textDecoration: 'underline' }}>Recommendations based on Favorites</h5>
         <p style={{ textAlign: 'center' }}>{user.visited}</p>
 
@@ -142,18 +154,79 @@ function UserProfile(props) {
 
         <p style={{ textAlign: 'center' }}>{visitedInfo}</p>
 
-        {/* Display the visited countries*/}
-
-        <h5 style = {{ textAlign: 'center', textDecoration: 'underline'}}>Some of my favorite places: </h5>
-        <div style = {{textAlign: 'center'}}>
+        <h5 style={{ textAlign: 'center', textDecoration: 'underline' }}>Some of my favorite places: </h5>
+        <div style={{ textAlign: 'center' }}>
           <BottomVisited cookies={cookies} setList={setList} dataList={dataList} allLocation={allLocation} selectList={selectList}
-           selectedList={selectedList} shareLink={shareLink} setShareLink={setShareLink} showShareList={showShareList}
+            selectedList={selectedList} shareLink={shareLink} setShareLink={setShareLink} showShareList={showShareList}
             setShareListModal={setShareListModal} shareList={shareList} />
 
         </div>
       </div>
     )
   }
-  return <NotLoggedIn />
+
+  const returnNormal = () => {
+    return (
+      <div>
+        <h1 style={{ textAlign: 'center', color: (214, 122, 127) }}>Welcome {user.name} </h1>
+        <br />
+
+        {/*<form onSubmit={() => handleEdit()} style={{ textAlign: 'center' }}>*/}
+        <form onSubmit={() => handleEdit()} style={{ textAlign: 'center' }}>
+
+          <input type="submit" value="Edit Profile" />
+        </form>
+
+        <br />
+
+        <h5 style={{ textAlign: 'center', textDecoration: 'underline' }}> About Me</h5>
+        <div style={{ textAlign: 'center' }}>
+          <ul style={{ textAlign: 'left', display: 'inline-block' }}>
+            <li>Name: {user.name}</li>
+            <li>Email: {user.email} </li>
+            <li>Location: {user.from_location}</li>
+          </ul>
+        </div>
+
+        <p style={{ textAlign: 'center', fontStyle: 'italic' }}>For the following text fields, please enter the information you want to display and press Submit <br />
+          "Refresh Browser to see changes applied"</p>
+
+        <h5 style={{ textAlign: 'center', textDecoration: 'underline' }}>Background and Interests</h5>
+        <p style={{ textAlign: 'center' }}>{user.background}</p>
+
+        <p style={{ textAlign: 'center' }}>{backgroundInfo}</p>
+
+        <h5 style={{ textAlign: 'center', textDecoration: 'underline' }}>Recommendations based on Favorites</h5>
+        <p style={{ textAlign: 'center' }}>{user.visited}</p>
+
+
+        <p style={{ textAlign: 'center' }}>{visitedInfo}</p>
+
+        <h5 style={{ textAlign: 'center', textDecoration: 'underline' }}>Some of my favorite places: </h5>
+        <div style={{ textAlign: 'center' }}>
+          <BottomVisited cookies={cookies} setList={setList} dataList={dataList} allLocation={allLocation} selectList={selectList}
+            selectedList={selectedList} shareLink={shareLink} setShareLink={setShareLink} showShareList={showShareList}
+            setShareListModal={setShareListModal} shareList={shareList} />
+
+        </div>
+      </div>
+    )
+  }
+
+  if (existsCookie) {
+    if(isInEditMode){
+      return(
+        <div>
+          {returnEditMode()}
+        </div>
+      )
+    }else{
+      return(
+        <div>
+          {returnNormal()}
+        </div>
+      )
+    }
+  } return <NotLoggedIn />
 }
 export default UserProfile;
