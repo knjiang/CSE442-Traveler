04590a5307d64a7d89d219ec48feded734c3f59d
Brian <briancis@buffalo.edu>
2021-11-15 10:57:05 -0500

    Made a backend for changing name and email #133/#126

--- backend/profiles/urls.py
+++ backend/profiles/urls.py
@@ -27,6 +27,8 @@ AddDescriptionView,
 DelDescriptionView,
 EditDescriptionView,
 GetDescriptionView,
+ChangeNameView,
+ChangeEmailView,
 ResetView
 )
 
@@ -56,5 +58,7 @@ urlpatterns = [
     path('add_visited_list/', AddVisitedView.as_view()),
     path('get_visited_data/', DeleteVisitedView.as_view()),
     path('delete_visited_data', GetVisitedDataView.as_view()), 
+    path('change_name', ChangeNameView.as_view()), 
+    path('delete_email', ChangeEmailView.as_view()), 
     path ('reset/', ResetView.as_view())
 ]
