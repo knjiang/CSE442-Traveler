b96fc81b22eb7cef7734a16ede80e6d39095eeac
Ken Jiang <jiangk@bxscience.edu>
2021-12-06 00:12:06 -0500

    Add frontend for tags #135

--- backend/forums/urls.py
+++ backend/forums/urls.py
@@ -8,7 +8,8 @@ from .views import (
     GetPostView, 
     DeletePostView, 
     DeleteCommentView,
-    AddEmojiToComment
+    AddEmojiToComment,
+    AddTag,
 )
 
 app_name = "forums"
@@ -22,5 +23,6 @@ urlpatterns = [
     path('get_comment/', GetCommentView.as_view()),
     path('del_post/', DeletePostView.as_view()),
     path('del_comment/', DeleteCommentView.as_view()),
-    path('add_emoji_to_comment/', AddEmojiToComment.as_view())
+    path('add_emoji_to_comment/', AddEmojiToComment.as_view()),
+    path('add_tag/', AddTag.as_view())
 ]
